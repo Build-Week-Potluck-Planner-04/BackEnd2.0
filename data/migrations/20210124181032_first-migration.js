4b5eaa20125a5e79c80af00acff4a9a50d73eb52
Angelique Abacajan <ma_utsey@yahoo.com>
2021-12-21 21:30:49 -0500

    take 2

--- data/migrations/20210124181032_first-migration.js
+++ data/migrations/20210124181032_first-migration.js
@@ -1,37 +1,37 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable("users", (users) => {
-      users.increments("user_id")
-      users.string("username", 200)
+    .createTable("users", (table) => {
+      table.increments("user_id")
+      table.string("username", 200)
         .notNullable()
         .unique()
-      users.string("password", 200)
+      table.string("password", 200)
         .notNullable()
-      users.string("role", 20)
+      table.string("role", 20)
         .notNullable()
     })
 
-    .createTable("foods", (foods) => {
-      foods.increments("food_id")
-      foods.string("food_name")
+    .createTable("foods", (table) => {
+      table.increments("food_id")
+      table.string("food_name")
     })
 
-    .createTable("guests", (guests) => {
-      guests.increments("guest_id")
-      guests.string("guest_name")
+    .createTable("guests", (table) => {
+      table.increments("guest_id")
+      table.string("guest_name")
     })
 
-    .createTable("potlucks", (potlucks) => {
-      potlucks.increments("potluck_id")
-      potlucks.string("potluckName")
+    .createTable("potlucks", (table) => {
+      table.increments("potluck_id")
+      table.string("potluckName")
         .notNullable()
-      potlucks.string("date")
+      table.string("date")
         .notNullable()
-      potlucks.string("time")
+      table.string("time")
         .notNullable()
-      potlucks.string("location")
+      table.string("location")
         .notNullable()
-      potlucks.integer("user_id")
+      table.integer("user_id")
         .unsigned()
         .notNullable()
         .references("user_id")
@@ -40,16 +40,16 @@ exports.up = async (knex) => {
         .onDelete("CASCADE")
     })
 
-    .createTable("potluck_foods", (potluck_foods) => {
-      potluck_foods.increments("potluck_food_id")
-      potluck_foods.integer("potluck_id")
+    .createTable("potluck_foods", (table) => {
+      table.increments("potluck_food_id")
+      table.integer("potluck_id")
         .unsigned()
         .notNullable()
         .references("potluck_id")
         .inTable("potlucks")
         .onUpdate("CASCADE")
         .onDelete("CASCADE")
-      potluck_foods.integer("food_id")
+      table.integer("food_id")
         .unsigned()
         .notNullable()
         .references("food_id")
@@ -58,16 +58,16 @@ exports.up = async (knex) => {
         .onDelete("CASCADE")
     })
 
-    .createTable("potluck_guests", (potluck_guests) => {
-      potluck_guests.increments("potluck_guest_id")
-      potluck_guests.integer("potluck_id")
+    .createTable("potluck_guests", (table) => {
+      table.increments("potluck_guest_id")
+      table.integer("potluck_id")
         .unsigned()
         .notNullable()
         .references("potluck_id")
         .inTable("potlucks")
         .onUpdate("CASCADE")
         .onDelete("CASCADE")
-      potluck_guests.integer("guest_id")
+      table.integer("guest_id")
         .unsigned()
         .notNullable()
         .references("guest_id")
